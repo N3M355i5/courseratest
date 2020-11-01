bbbe4d0a72047e7aa55bb41fe5d57af327d0eedc
N3M355i5 <aryanvarshney1@gmail.com>
2020-11-01 19:11:44 +0530

    update final

---
+++ Week 5/js/script.js
@@ -0,0 +1,31 @@
+// //DOM Manipulation
+// document.addEventListener("DOMContentLoader", 
+// 	function(event) {
+// // function sayHello(event) {
+// // 	var name = document.getElementById("name").value;
+
+// // 	// alert("Hello! " + name);
+// // 	document
+// // 			.getElementById("content")
+// // 			.innerHTML = "<h2>Hello " + name + "</h2>";
+// // 	if(name === "student") {
+// // 		var title = document
+// // 							.getElementById("title")
+// // 							.textContent;
+// // 		title += " & its fine enough";
+// // 		document
+// // 				.getElementById("title")
+// // 				.textContent = title;
+// // 	}
+// // }
+// // document.querySelector("button").addEventListener("click", sayHello);
+// document.querySelector("body")
+// 	.addEventListener("mousemove", 
+// 		function (event) {
+// 			if(event.shiftKey == true) {
+// 				console.log("x: " + event.clientX);
+// 				console.log("y: " + event.clientY);
+// 			}
+// 		}
+// 	);
+// });
